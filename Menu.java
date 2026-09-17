4aeed8cc122ca73e8519d2273c2aff0334be997c
Pedro Victor <pedro.kent2010@gmail.com>
2026-09-17 20:30:58 -0300

    feat: adição de metodos para teste de persistência

--- Menu.java
+++ Menu.java
@@ -24,6 +24,9 @@ public class Menu {
             System.out.println("2. Retirar veículo");
             System.out.println("3. Listar vagas");
             System.out.println("4. Listar registros");
+            System.out.println("5. Consultar vaga");
+            System.out.println("6. Listar veículos estacionados");
+            System.out.println("7. Mostrar faturamento");
             System.out.println("0. Salvar e sair");
             System.out.println("====================================");
             System.out.print("Escolha uma opção: ");
@@ -61,39 +64,39 @@ public class Menu {
     }
 
     private void estacionarVeiculo() {
-        System.out.println("Digite a placa do carro: ");
+        System.out.print("Digite a placa do carro: ");
         String placa = scanner.nextLine().toUpperCase();
 
-        System.out.println("Digite o modelo do veículo: ");
+        System.out.print("Digite o modelo do veículo: ");
         String modelo = scanner.nextLine().toUpperCase();
 
-        System.out.println("Digite o tipo do veículo (carro ou moto): ");
+        System.out.print("Digite o tipo do veículo (carro ou moto): ");
         TipoVeiculo tipoVeiculo = TipoVeiculo.valueOf(scanner.nextLine().toUpperCase());
 
         Veiculo veiculo = new Veiculo(placa, modelo, tipoVeiculo);
 
         try {
             estacionamento.estacionar(veiculo);
-            System.out.println("Seu veículo foi estacionado com sucesso!");
+            System.out.println("Seu veículo foi estacionado com sucesso!\n");
         } catch (IllegalArgumentException | IllegalStateException e) {
-            System.out.println("Não foi possível estacionar o veículo");
-            System.out.println(e.getMessage());
+            System.out.println("\nNão foi possível estacionar o veículo");
+            System.out.println(e.getMessage() + "\n");
         }
     }
 
     private void retirarVeiculo() {
-        System.out.println("Digite a placa do veículo a ser retirado: ");
+        System.out.print("Digite a placa do veículo a ser retirado: ");
         String placa = scanner.nextLine();
 
         try {
 
             double valor = estacionamento.retirarVeiculo(placa);
             System.out.println("Veículo retirado com sucesso");
-            System.out.println("Valor a ser pago: R$ " + valor);
+            System.out.println("Valor a ser pago: R$ " + valor + "\n");
 
         } catch (IllegalArgumentException | IllegalStateException e) {
             System.out.println("Não foi possível retirar o veículo");
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage() + "\n");
         }
     }
 
@@ -113,6 +116,7 @@ public class Menu {
         for (Vaga vaga : vagasOcupadas) {
             System.out.println(vaga);
         }
+        System.out.println("");
     }
 
     private void listarRegistros() {
@@ -122,11 +126,40 @@ public class Menu {
         System.out.println("========== REGISTROS ==========");
 
         if (registros.isEmpty()) {
-            System.out.println("\nNão há registros");
+            System.out.println("\nNão há registros\n");
+            return;
         }
+
         for (RegistroEstacionamento registro : registros) {
             System.out.println(registro);
         }
     }
 
+    private void consultarVaga() {
+        System.out.print("Digite o número da vaga para consulta: ");
+        Vaga vaga = estacionamento.buscarVagaPorNumero(scanner.nextInt());
+
+        if (vaga.estaLivre()) {
+            System.out.println("A vaga " + vaga.getNumero() + " está livre!\n");
+        } else {
+            System.out.println("A vaga " + vaga.getNumero() + " está ocupada\n");
+        }
+
+    }
+
+    private void listarVeiculosEstacionados() {
+        List<Vaga> vagas = estacionamento.listarVagasOcupadas();
+
+        System.out.println("Veículos estacionados:");
+
+        for (Vaga vaga : vagas) {
+            System.out.println(vaga.getVeiculo().getModelo());
+        }
+        System.out.println("");
+    }
+
+    private void mostrarFaturamento() {
+        System.out.println("========== FATURAMENTO ==========");
+        System.out.println("\nR$: " + estacionamento.calcularFaturamento() + "\n");
+    }
 }

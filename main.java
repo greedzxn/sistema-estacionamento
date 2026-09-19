293a743eee376a5d1e3478b135e643445c1837a0
Pedro Victor <pedro.kent2010@gmail.com>
2026-09-18 21:36:49 -0300

    fix: nome da classe incorreto

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ package estacionamento;
 import java.util.List;
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
